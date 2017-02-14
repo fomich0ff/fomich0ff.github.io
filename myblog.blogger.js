1864d91853a392261f5f62d12c05a1ed0bb51fa1
Oleg Fomichev <o.g.fomichev@outlook.com>
2017-02-14 13:58:56 -0600

    menu js changed

--- myblog.blogger.js
+++ myblog.blogger.js
@@ -1,5 +1,16 @@
-$('#nav').affix({
-    offset: {
-        top: $('header').height()
-    }
+$(document).ready(() => {
+    $('#nav').affix({
+        offset: {
+            top: $('header').height()
+        }
+    });
+    
+    $.each(pages, (i, v) => {
+        
+        var html = "<li>";
+        html += v.selected ? "<span>" + v.title + "</span>" : "<a href='" + v.url + "'>" + v.title + "</a>";
+        var a = $("#nav ul.navbar-nav").add(html); 
+        if(v.selected)
+            a.addClass("active");
+    });
 });
