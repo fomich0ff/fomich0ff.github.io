c1beeaac7a6b0bf85ccb25b88ad7dfed8cbe1beb
fomich0ff <o.g.fomichev@outlook.com>
2017-02-14 16:00:26 -0600

    Update myblog.blogger.js

--- myblog.blogger.js
+++ myblog.blogger.js
@@ -9,7 +9,7 @@ $(document).ready(() => {
         
         var html = "<li>";
         html += v.selected ? "<span class='page'>" + v.title + "</span>" : ( 
-            v.post ? "<span class='post'>" + v.title + "</span>" : "<a href='" + v.url + "'>" + v.title + "</a>";
+            v.post ? "<span class='post'>" + v.title + "</span>" : "<a href='" + v.url + "'>" + v.title + "</a>");
         html += "</li>";
         $("#main-menu").append(html); 
     });
