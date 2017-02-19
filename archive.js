4ec7382feb3124a250731b10f9ffd5e179ab39d7
Oleg Fomichev <o.g.fomichev@outlook.com>
2017-02-18 21:22:47 -0600

    fix

--- archive.js
+++ archive.js
@@ -1,75 +1,86 @@
-$(document).ready(()=>{
-	$.getJSON( "/feeds/posts/default?alt=json", function( data ) {
+$(document).ready(() => {
+	$.getJSON("/feeds/posts/default?alt=json", function (data) {
 
-    		var years = [];
-		var monthNames = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
-		$.each(data.feed.entry, (index, value)=>{
+			var years = [];
+			var monthNames = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+			$.each(data.feed.entry, (index, value) => {
 
-			var published = new Date(value.published.$t);
+					var published = new Date(value.published.$t);
 
-			/* get year */
-			var year = published.getFullYear().toString();
+					/* get year */
+					var year = published.getFullYear().toString();
 
-			var filtered = $(years).filter((index, value) => {
-				return value.year == year;
+					var filtered = $(years).filter((index, value) => {
+						return value.year == year;
+					});
+
+					var foundYear = filtered.length > 0 ? filtered[0] : null;
+
+					if (!foundYear) {
+						years.push({
+							"year": year,
+							"months": []
+						});
+						foundYear = years[years.length - 1];
+					}
+
+					var yearIndex = years.indexOf(foundYear);
+
+					/* get month */
+					var month = monthNames[published.getMonth()];
+					var foundMonth = filtered.length > 0 ? filtered[0] : null;
+					filtered = $(years[yearIndex].months, (index, item) => {
+						return item.month == month;
+					});
+
+					if (!foundMonth) {
+						years[yearIndex].months.push({
+							'month': month,
+							'days': []
+						}));
+
+					foundMonth = years[yearIndex].months[years[yearIndex].months.length - 1];
+				}
+
+				var monthIndex = years[yearIndex].months.indexOf(foundMonth);
+
+				/*get day*/
+				var day = published.getDay(); filtered = $(years[yearIndex].months[monthIndex]).filter((index, item) => {
+					return item.day == day;
+				})
+
+				var foundDay = filtered.length > 0 ? filtered[0] : null;
+
+				if (!foundDay) {
+					years[yearIndex].months[monthIndex].days.push({
+						"day": day,
+						"posts": []
+					});
+					foundDay = years[yearIndex].months[monthIndex].days[years[yearIndex].months[monthIndex].days.length - 1];
+				}
+
+
+				var dayIndex = years[yearIndex].months[monthIndex].days.indexOf(foundDay);
+
+				years[yearIndex].months[monthIndex].days[dayIndex].posts.push({
+					"title": value.title.$t,
+					"link": value.link.filter((item) => {
+						return item.rel == 'alternate';
+					})[0].href
+				});
+
 			});
 
-			var foundYear = filtered.length > 0 ? filtered[0] : null;
 
-			if(!foundYear) {
-				years.push({
-					"year" : year,
-					"months": []
-				});
-				foundYear = years[years.length-1];
-			}
-
-			var yearIndex = years.indexOf(foundYear);      
-
-			/* get month */
-			var month = monthNames[published.getMonth()];
-
-			if(years[yearIndex].months[month] === undefined){
-				years[yearIndex].months[month] = [];
-			}
-
-			/*get day*/
-			var day = published.getDay();
-			filtered = $(years[yearIndex].months[month]).filter((index, item) => {
-				return item.day == day;
-			})
-
-			var foundDay = filtered.length > 0 ? filtered[0] : null;
-
-			if (!foundDay)
-			{
-				years[yearIndex].months[month].push({
-					"day":day,
-					"posts": []
-				});
-				foundDay = years[yearIndex].months[month][years[yearIndex].months[month].length - 1];
-			}
-
-    
-			var dayIndex = years[yearIndex].months[month].indexOf(foundDay);
-
-			years[yearIndex].months[month][dayIndex].posts.push({
-				"title" : value.title.$t,
-				"link" : value.link.filter((item) => { return item.rel == 'alternate'; })[0].href
-			});
-
-		});
-
-
-		var ViewModel = function(data) {
+		var ViewModel = function (data) {
 			this.years = ko.observable(data.posts);
 			this.labels = ko.observable(data.labels);
 		};
- 
+
 		ko.applyBindings(new ViewModel({
-			"posts" : years,
+			"posts": years,
 			"labels": labels
-		})); 
+		}));
 
 		console.debug(data, years);
 	});
