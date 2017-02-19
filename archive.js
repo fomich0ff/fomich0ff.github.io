4d0dca402eb933cf7f4ea93e84aeba4ac32d53f8
fomich0ff <fomich0ff@debian>
2017-02-18 19:58:46 -0600

    fix

--- archive.js
+++ archive.js
@@ -1,73 +1,72 @@
 $(document).ready(()=>{
 	$.getJSON( "/feeds/posts/default?alt=json", function( data ) {
 
-    var years = [];
-    var monthNames = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
-    $.each(data.feed.entry, (index, value)=>{
+    		var years = [];
+		var monthNames = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+		$.each(data.feed.entry, (index, value)=>{
 
-      var published = new Date(value.published.$t);
+			var published = new Date(value.published.$t);
 
-      /* get year */
-      var year = published.getFullYear().toString();
+			/* get year */
+			var year = published.getFullYear().toString();
 
-	var filtered = $(years).filter((index, value) => {
-		return value.year == year;
-	});
+			var filtered = $(years).filter((index, value) => {
+				return value.year == year;
+			});
 
-	var foundYear = filtered.length > 0 ? filtered[0] : null;
+			var foundYear = filtered.length > 0 ? filtered[0] : null;
 
-      if(!foundYear) {
-         years.push({
-           "year" : year,
-           "months": []
-         });
+			if(!foundYear) {
+				years.push({
+					"year" : year,
+					"months": []
+				});
+				foundYear = years[years.length-1];
+			}
 
-	foundYear = years[years.length-1];
-      
+			var yearIndex = years.indexOf(foundYear);      
 
-      var yearIndex = years.indexOf(foundYear);      
+			/* get month */
+			var month = monthNames[published.getMonth()];
 
-      /* get month */
-      var month = monthNames[published.getMonth()];
+			if(years[yearIndex].months[month] === undefined){
+				years[yearIndex].months[month] = [];
+			}
 
-      if(years[yearIndex].months[month] === undefined){
-         years[yearIndex].months[month] = [];
-      }
+			/*get day*/
+			var day = published.getDay();
+			filtered = $(years[yearIndex].months[month]).filter((index, item) => {
+				return item.day == day;
+			})
 
-      /*get day*/
-      var day = published.getDay();
-	filtered = $(years[yearIndex].months[month]).filter((index, item) => {
-		return item.day == day;
-	})
+			var foundDay = filtered.length > 0 ? filtered[0] : null;
 
-	var foundDay = filtered.length > 0 ? filtered[0] : null;
-
-	if (!foundDay)
-	{
-		years[yearIndex].months[month].push({
-			"day":day,
-			"posts": []
-		});
-		foundDay = years[yearIndex].months[month][years[yearIndex].months[month].length - 1];
-	}
+			if (!foundDay)
+			{
+				years[yearIndex].months[month].push({
+					"day":day,
+					"posts": []
+				});
+				foundDay = years[yearIndex].months[month][years[yearIndex].months[month].length - 1];
+			}
 
     
-	var dayIndex = years[yearIndex][month].indexOf(foundDay);
+			var dayIndex = years[yearIndex][month].indexOf(foundDay);
 
-	years[yearIndex][month][dayIndex].push({
-		"title" : value.title.$t,
-		"link" : value.link.filter((item) => { return item.rel == 'alternate'; })[0].href
-	});
+			years[yearIndex][month][dayIndex].push({
+				"title" : value.title.$t,
+				"link" : value.link.filter((item) => { return item.rel == 'alternate'; })[0].href
+			});
 
-});
+		});
 
 
-    var ViewModel = function(data) {
-       this.data = ko.observable(data);
-    };
+		var ViewModel = function(data) {
+			this.data = ko.observable(data);
+		};
  
-    ko.applyBindings(new ViewModel(years)); 
+		ko.applyBindings(new ViewModel(years)); 
 
-    console.debug(data, years);
-  });
+		console.debug(data, years);
+	});
 });
