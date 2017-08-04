d5da903dbeb5907c7143f2763a5776d5cabddfdc
Oleg Fomichev <o.g.fomichev@outlook.com>
2017-08-04 11:09:45 -0500

    atom feed added
    old blog stuff moved

--- old-blog/myblog.blogger.js
+++ old-blog/myblog.blogger.js
@@ -4,17 +4,17 @@ $(document).ready(() => {
             top: $('header').height()
         }
     });*/
-    
+
     $('span.post-label:contains("archive")').addClass('archive-label');
-    
+
     $.each(pages, (i, v) => {
-        
+
         var html = "<li>";
-        html += v.selected ? "<span class='page'>" + v.title + "</span>" : ( 
+        html += v.selected ? "<span class='page'>" + v.title + "</span>" : (
             v.post ? "<span class='post'>" + v.title + "</span>" : "<a href='" + v.url + "'>" + v.title + "</a>");
         html += "</li>";
-        $("#main-menu").append(html); 
+        $("#main-menu").append(html);
     });
-    
-    
+
+
 });
